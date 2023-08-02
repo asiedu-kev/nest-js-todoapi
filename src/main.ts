46da2a744546a6e8ca13fbfd44fcc87399d692d4
asiedu-kev <asiedukevin050@gmail.com>
2023-08-02 02:45:06 +0100

    :bug: Add dotenv loading fro the app

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as dotenv from 'dotenv'
 
 async function bootstrap() {
+  // Load the environment variables from .env file
+  dotenv.config()
   const app = await NestFactory.create(AppModule, { abortOnError: false })
   app.useGlobalPipes(new ValidationPipe())
   const options = new DocumentBuilder()
